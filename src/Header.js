ac170f73c72f8a56abf08d75fcd189ac36c7aebc
HossamMarey <hossam.marey@inovola.com>
2022-02-21 14:19:45 +0200

    add counter slice

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { handleLogin, handleLogout } from "./redux/features/authSlice";
 
-export default function Header({
-  counter,
-  isLogged,
-  handleLogin,
-  handleLogout,
-}) {
+export default function Header() {
+  const dispatch = useDispatch();
+  const isLogged = useSelector((state) => state.auth.isLogged);
+  const counter = useSelector((state) => state.counter.value);
+
   return (
     <header>
       <div> {counter} </div>
       {isLogged ? (
         <>
           <strong> Hello </strong>
-          <button onClick={handleLogout}> logout </button>
+          <button onClick={() => dispatch(handleLogout())}> logout </button>
         </>
       ) : (
-        <button onClick={handleLogin}> login </button>
+        <button onClick={() => dispatch(handleLogin())}> login </button>
       )}
     </header>
   );
